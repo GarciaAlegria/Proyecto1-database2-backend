6bf05174876f8cf07fc059799a48cf965bae496d
GarciaAlegria <abnerivan2003@gmail.com>
2024-02-16 17:06:20 -0600

    conectar con mongo atlas listo

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const examenesRoutes = require('./src/examenes/routes')
 const reviewsRoutes = require('./src/reviews/routes')
 const requestsRoutes = require('./src/requests/routes')
 const serviciosRoutes = require('./src/servicios/routes')
+const mongoose = require('mongoose')
+require("dotenv").config();
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(cors({
     origin: '*'
 }))
+
+// mongodb atlas connection
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to mongodb atlas'))
+.catch((error) => console.error(error));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
